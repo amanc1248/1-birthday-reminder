892eafe46b7770ffd19100a00e5e91ada68051eb
amanc1248 <amanc1248@gmail.com>
2021-03-04 07:57:55 +0545

    ADD: 4 project accordion

--- 4-accordion/setup/src/Question.js
+++ 4-accordion/setup/src/Question.js
@@ -1,7 +1,31 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+const Question = ({ id, title, info }) => {
+  const [answerToogle, setAnswerToogle] = useState(false);
+  const showHideAnswer = () => {
+    setAnswerToogle(!answerToogle);
+  };
+  return (
+    <article className="question">
+      <h4>{title}</h4>
+      <button className="btn">
+        {answerToogle ? (
+          <AiOutlineMinus
+            onClick={() => {
+              showHideAnswer();
+            }}
+          ></AiOutlineMinus>
+        ) : (
+          <AiOutlinePlus
+            onClick={() => {
+              showHideAnswer();
+            }}
+          ></AiOutlinePlus>
+        )}
+      </button>
+      {answerToogle ? <p>{info}</p> : ""}
+    </article>
+  );
 };
 
 export default Question;
