4a155f3b26aacc4058abbe6033f45e8f31b9fd03
amanc1248 <amanc1248@gmail.com>
2021-03-04 10:15:34 +0545

    ADD: 5th Project Menu Items

--- 5-menu/setup/src/App.js
+++ 5-menu/setup/src/App.js
@@ -1,10 +1,35 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
 
+// 👇added spread operater and set method here
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+console.log("====================================");
+console.log(allCategories);
+console.log("====================================");
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+  const filterItems = (category) => {
+    let newItems = items.filter((item) => item.category === category);
+    return setMenuItems(newItems);
+  };
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>Our Menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories
+          filterItems={filterItems}
+          categories={categories}
+        ></Categories>
+        <Menu items={menuItems}></Menu>
+      </section>
+    </main>
+  );
 }
 
 export default App;
